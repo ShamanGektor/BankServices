ef077e83ef49424e2015cbe4d864f7529c74c24c
Zhadan Victor <zhadan.viktor@gmail.com>
2021-06-04 13:55:30 +0300

    Add manager

---
+++ Manager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankServices
+{
+    class Manager
+    {
+        public void ShowListContracts()
+        {
+        }
+
+        public void ShowtModifiedListContracts()
+        {
+
+        }
+
+        public void ConfirmContract()
+        {
+
+        }
+
+        public void DeclineContract()
+        {
+
+        }
+
+        public void EditContract()
+        {
+
+        }
+    }
+}
